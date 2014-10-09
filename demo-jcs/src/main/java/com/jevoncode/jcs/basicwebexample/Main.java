e2d6059d4fa16465bbc79cc9f2de0a9de46aedc0
JevonCode <jevoncode@gmail.com>
2014-10-09 17:43:04 +0800

    fix ClassNotFoundException and test DC

--- demo-jcs/src/main/java/com/jevoncode/jcs/basicwebexample/Main.java
+++ demo-jcs/src/main/java/com/jevoncode/jcs/basicwebexample/Main.java
@@ -1,8 +1,14 @@
 package com.jevoncode.jcs.basicwebexample;
-
+/**
+ * VM args:-Dcom.sun.management.jmxremote
+ * @author jevoncode
+ *
+ */
 public class Main {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		Thread.sleep(15000);
 		BookVObjManager bookVObjManager = BookVObjManager.getInstance();
+//		BookVObjManagerDefault bookVObjManager = BookVObjManagerDefault.getInstance();
 		int i= 0;
 		while(true){
 			bookVObjManager.getBookVObj(i++);
