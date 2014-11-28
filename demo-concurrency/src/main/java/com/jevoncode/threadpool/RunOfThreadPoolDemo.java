9ea664d49bfb2741a45c747f6f1adf893b09b0f8
JevonCode <jevoncode@gmail.com>
2014-11-28 11:20:14 +0800

    实践任务个数远远大于线程池的线程数及其处理速度时会怎样？情况一：内存溢出;情况二：请查看toThink.txt文件。

---
+++ demo-concurrency/src/main/java/com/jevoncode/threadpool/RunOfThreadPoolDemo.java
@@ -0,0 +1,33 @@
+package com.jevoncode.threadpool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import org.apache.log4j.Logger;
+
+import com.jevoncode.tools.JVMTool;
+
+/**
+ * 试验下,如果队列的任务远远大于线程池及其处理速度会怎样?
+ * 毫无意外:内存溢出 Exception in thread "main" java.lang.OutOfMemoryError: Java heap space
+ * @author jevoncode
+ * vm args:-Xms20m -Xmx20m
+ */
+public class RunOfThreadPoolDemo {
+	public static Logger log = Logger.getLogger(RunOfThreadPoolDemo.class);
+	public static void main(String[] args) throws InterruptedException {
+		Thread.sleep(10000);
+//		ExecutorService executorService = Executors.newFixedThreadPool(10);
+		ThreadPoolExecutor executorService = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
+		int i=1;
+		while(true){
+			if(i<10)
+			Thread.sleep(1000);
+			executorService.submit(new Task(i++));
+			log.debug("线程队列大小 ："+executorService.getQueue().size());
+			log.debug(JVMTool.memoryStatusPlain());
+		}
+	}
+}
---
+++ demo-concurrency/src/main/java/com/jevoncode/threadpool/Task.java
@@ -0,0 +1,35 @@
+package com.jevoncode.threadpool;
+
+import org.apache.log4j.Logger;
+
+/**
+ * 使用线程池执行该任务
+ * @author jevoncode
+ *
+ */
+public class Task implements Runnable{
+
+	public static Logger log = Logger.getLogger(Task.class);
+	public int i;
+	public Task(int i) {
+		super();
+		this.i = i;
+	}
+	public void run() {
+		log.debug(Thread.currentThread().getName()+" is running!---Task"+i);
+		
+		//占10M内存,奇怪,加了这句,执行该任务的线程就去跑去执行队列里的其他任务,这是为什么呢???????
+		//不加则执行该任务的线程就会继续sleep,不会理会队列里的任务,直至内存溢出
+		byte[] block = new byte[1024*1024*10]; 
+		
+//		byte[] block = new byte[1024*1024*1]; //但如果加这句,1M内存的，不会造成10M那条语句的问题。
+		
+		try {
+			Thread.sleep(999999999);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
